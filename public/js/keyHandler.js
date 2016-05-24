b13d4e408f9e9dacdc3c13919945721434891093
Courtney Nguyen <cnguyen@1800contacts.com>
2016-05-24 00:53:09 -0600

    only one call at a time

--- public/js/keyHandler.js
+++ public/js/keyHandler.js
@@ -3,18 +3,32 @@ var KeyHandler = new function(){
   this.actionInProgress = false;
   this.setKeyDownAction = function(key, action){
     if(this.keyMap[key] !== null){
-      this.keyMap[key].keyDownAction = action;
+      this.keyMap[key] = {
+        keyDownAction: action,
+        pressed: false,
+        wasPressed: false
+      };
     }
   };
 
   this.handleKeyDown = function(event){
     var key = String.fromCharCode(event.keyCode).toLowerCase();
-    if(this.keyMap[key] && this.keyMap[key].keyDownAction && this.actionInProgress === false){
-      this.actionInProgress = true;
-      this.keyMap[key].keyDownAction();
-      this.actionInProgress = false;
+    if(this.keyMap[key] && this.keyMap[key].keyDownAction){
+      if(!this.keyMap[key].pressed){
+        this.keyMap[key].keyDownAction();
+        this.keyMap[key].wasPressed = false;
+        this.keyMap[key].pressed = true;
+      }
     }
   };
+
+  this.handleKeyRelease = function(key){
+    var key = String.fromCharCode(event.keyCode).toLowerCase();
+    if(this.keyMap[key] && this.keyMap[key].pressed){
+      this.keyMap[key].wasPressed = true;
+      this.keyMap[key].pressed = false;
+    }
+  }
 
   this.completeAction = function(){
     this.actionInProgress = false;
@@ -22,3 +36,4 @@ var KeyHandler = new function(){
 }
 
 document.addEventListener('keydown', KeyHandler.handleKeyDown.bind(KeyHandler));
+document.addEventListener('keyup', KeyHandler.handleKeyRelease.bind(KeyHandler));
